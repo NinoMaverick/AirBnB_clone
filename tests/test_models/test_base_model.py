3c3115d1897f8714beb6b0aa4d5fa380e445c6d1
Gogo-Chinda Progress <progressgogochinda@gmail.com>
2024-03-12 03:26:25 -0700

    check

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,10 +11,10 @@ from ..models.base_model import BaseModel
 def test_to_dict(self):
     """send to dict"""
     base = BaseModel()
-    base.name = "labrat"
+    base.name = "Gogo"
     base.age = 25
     dic = base.to_dict()
-    self.assertEqual(dic["id"], rat.id)
+    self.assertEqual(dic["id"], rbase.id)
     self.assertEqual(dic["__class__"], type(base).__name__)
     self.assertEqual(dic["created_at"], base.created_at.isoformat())
     self.assertEqual(dic["updated_at"], base.updated_at.isoformat())
